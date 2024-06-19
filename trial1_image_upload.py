5e2f39f97989b892431302b89cdf5f55906b7f9a
Pranav Goyal <83721271+goy4l@users.noreply.github.com>
2024-06-19 13:56:48 +0530

    Create multimodal_retrieval.py

--- trial1_image_upload.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-uploaded_file = st.file_uploader("Upload your image...", type=["jpg","jpeg","png","pdf"])
-
-if uploaded_file is not None:
-    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
